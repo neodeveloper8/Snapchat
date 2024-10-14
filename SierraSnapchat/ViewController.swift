e41040304d350fbb4a10985fb497d7aeadded100
neodeveloper8 <gonzaloast8@gmail.com>
2024-10-14 17:49:58 -0500

    Initial Commit

---
+++ SierraSnapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SierraSnapchat
+//
+//  Created by Gonzalo Alonso Sierra Tapia on 14/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
